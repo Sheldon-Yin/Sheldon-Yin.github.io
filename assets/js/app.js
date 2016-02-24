495d6fd7bd6e84eff59fbc09d37bd9b6e6613db8
xiedong <383541990@qq.com>
2016-02-24 14:30:52 +0800

    Android-01 new

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
-  alert("hello world");
-if(window.location.href=="http://www.sheldonyin.website/")
-{
-$("#sidebar").css({width:'100%'});
-$("#btnblog").click(function(){
-$("#sidebar").animate({width:'33.3333%'},'slow');
-});
-$("#btnabout").click(function(){
-$("#sidebar").animate({width:'0'},'slow');
-});
-}
+$(document).ready(function () {
+    console.log("hello world！欢迎你们到我的小站，这里也许还比较简陋，请不要介意~有改进的意见欢迎告诉我~留言板功能正在开发。也可以联系我：383541990@qq.com");
+    if (window.location.href == "http://www.sheldonyin.website/") {
+        $("#sidebar").css({width: '100%'});
+        $("#btnblog").click(function () {
+            $("#sidebar").animate({width: '33.3333%'}, 'slow');
+        });
+    }
+    if (window.location.href == "http://www.sheldonyin.website/about/") {
+        $("#navbarxs").addClass("hidden");
+        $("#sidebar").addClass("hidden");
+        $("#container").removeClass("col-sm-8 col-sm-offset-4");
+        $("#container").addClass("col-sm-12");
+    }
 });
