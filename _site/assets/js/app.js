54ea78ee3eb9a2d09012f2acfa8a0a60d947c244
xiedong <383541990@qq.com>
2016-02-24 13:43:21 +0800

    Android-01 new

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
   alert("hello world");
-if(window.location.href=="http://blog.ankanbiswas.in/Sheldon-Yin/")
+if(window.location.href=="http://www.sheldonyin.website/")
 {
 $("#sidebar").css({width:'100%'});
 $("#btnblog").click(function(){
 $("#sidebar").animate({width:'33.3333%'},'slow');
+});
+$("#btnabout").click(function(){
+$("#sidebar").animate({width:'0'},'slow');
 });
 }
 });
