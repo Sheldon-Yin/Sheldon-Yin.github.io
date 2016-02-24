e9164bcb67510ae4dcdaf2b4f24e7d1cda3a40f0
xiedong <383541990@qq.com>
2016-02-24 16:33:52 +0800

    Android-01 new

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,15 +1,9 @@
 $(document).ready(function () {
     console.log("hello world！欢迎你们到我的小站，这里也许还比较简陋，请不要介意~有改进的意见欢迎告诉我~留言板功能正在开发。也可以联系我：383541990@qq.com");
-    if (window.location.href == "http://www.sheldonyin.website/") {
+    if (window.location.href == "http://127.0.0.1:4000/") {
         $("#sidebar").css({width: '100%'});
         $("#btnblog").click(function () {
             $("#sidebar").animate({width: '33.3333%'}, 'slow');
         });
-    }
-    if (window.location.href == "http://www.sheldonyin.website/about/") {
-        $("#navbarxs").addClass("hidden");
-        $("#sidebar").addClass("hidden");
-        $("#container").removeClass("col-sm-8 col-sm-offset-4");
-        $("#container").addClass("col-sm-12");
     }
 });
